02f15f562ca7c0eba50873d848994e72d4dfb1c3
glmck13 <glmck13@verizon.net>
2019-07-03 14:56:54 -0400

    Add files via upload

---
+++ cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/ksh
+
+PATH=$PWD:~welby/bin:$PATH
+
+cd ~www-data/run/vitalsigns
+VITALRUN=$PWD
+WARNDAY=${1:-45}
+GRACEDAYS=${2:-5}
+let PURGEDAY=$WARNDAY+$GRACEDAYS
+
+find . -daystart -name lastheard -mtime $WARNDAY | while IFS='/' read x user x
+do
+	. ./$user/info.conf
+	sendaway.sh "$Email" "Vital Signs pending account deletion" "$Name - You have not accessed your Vital Signs account in the last $WARNDAY days.  If you want to retain your account, please access Vital Signs within the next $GRACEDAYS days, otherwise your account will be deleted.  If you have recommendations for improving the skill, please feel free to respond to this email with your suggestions.  Thanks for using Vital Signs!"
+done
+
+find . -daystart -name lastheard -mtime +$PURGEDAY | while IFS='/' read x user x
+do
+	rm -fr ./$user
+done
