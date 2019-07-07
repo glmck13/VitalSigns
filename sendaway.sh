39df87d0da1f483b4ae42f647dd18d800793744e
glmck13 <glmck13@verizon.net>
2019-07-07 12:08:02 -0400

    Add files via upload

---
+++ sendaway.sh
@@ -0,0 +1,20 @@
+#!/bin/ksh
+
+#
+# alpine config: https://github.com/termux/termux-packages/issues/2023
+# alpine IMAP auth: disable-these-authenticators=PLAIN
+#
+
+EMAIL=${1:?Enter address}
+SUBJECT=${2:?Enter subject}
+MESSAGE=${3:?Enter message}
+ATTACH=${4}
+
+export TERM=xterm
+expect >/dev/null <<EOF
+set timeout 120
+spawn alpine "$EMAIL"
+expect "To AddrBk"
+send "\r\r$ATTACH\r$SUBJECT\r$MESSAGE\rY"
+expect "Alpine finished"
+EOF
